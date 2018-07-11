378a4edbf6c92db00f252982384ceaf637fc414d
El Che <s9926149@gmail.com>
2018-07-11 18:46:34 +0800

    Add HikariCP MySQL dataSource settings
    Also add some code to verify the settings, this should be removed later.
    
    To run the project, you must create correspond settings,
    which can achieve by copy the template and remove the suffix `template`,
    then fill in the settings correspond to your environment.

---
+++ setup/database.md
@@ -0,0 +1,21 @@
+# Database setups
+
+```sql
+CREATE DATABASE mgb
+    DEFAULT character set utf8mb4
+    DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE TABLE test
+    (
+        id INT,
+        msg VARCHAR(512),
+        createDate DATETIME
+    )
+    ENGINE = InnoDB;
+
+CREATE USER 'the_user_name'@'localhost' IDENTIFIED BY 'the_pass_word';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON mgb.* TO 'the_user_name'@'the_pass_word';
+```
+
+which can achieve by copy the template and remove the suffix `template`,
--- src/main/java/org/cynerds/mgb/controller/GreetingController.java
+++ src/main/java/org/cynerds/mgb/controller/GreetingController.java
@@ -3,10 +3,18 @@ package org.cynerds.mgb.controller;
 import org.cynerds.mgb.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,8 +24,13 @@ public class GreetingController {
     private static final Logger LOG = LoggerFactory.getLogger(GreetingController.class);
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws SQLException {
+        LOG.warn("DS = {}", dataSource);
+        LOG.warn("conn = {}", dataSource.getConnection());
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
